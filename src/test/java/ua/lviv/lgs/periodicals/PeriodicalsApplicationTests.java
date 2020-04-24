7c98b6b4500913f65143da061c10b03d0beac638
Volodymyr Tymtsiv <vtymtsiv@hubspot.com>
2020-04-24 20:00:55 +0300

    Initial structure

---
+++ src/test/java/ua/lviv/lgs/periodicals/PeriodicalsApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.lgs.periodicals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeriodicalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
